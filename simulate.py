e39455576b71300ce69f621a301070f0c80b5f5d
jhmarche <jhmarche@uvm.edu>
2022-01-20 18:27:45 -0500

    Added a floor to the world.

--- simulate.py
+++ simulate.py
@@ -1,12 +1,15 @@
 import pybullet as p
 import time
+import pybullet_data
 
 # physics client for pybullet
 physicsClient = p.connect(p.GUI)
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
 
 p.setGravity(0, 0, -9.8)
 # reads in world described in box.sdf
 p.loadSDF("box.sdf")
+planeId = p.loadURDF("plane.urdf")
 
 # for loop to make simulation last longer
 for x in range(1000):
