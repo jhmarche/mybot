5a243b5513f6255c76dd48051b8a3d73610a7b3a
jhmarche <jhmarche@uvm.edu>
2022-01-20 18:30:54 -0500

    Set dimensions of box as variables.

--- generate.py
+++ generate.py
@@ -1,7 +1,15 @@
 import pyrosim.pyrosim as pyrosim
 
+# file to store information about world
 pyrosim.Start_SDF("box.sdf")
 
-pyrosim.Send_Cube(name="Box", pos=[0, 0, 0.5], size=[1, 1, 1])
+#set size of cube
+length = 1
+width = 1
+height = 1
 
+# parameters for the box
+pyrosim.Send_Cube(name="Box", pos=[0, 0, 0.5], size=[length, width, height])
+
+# stop pyrosim
 pyrosim.End()
