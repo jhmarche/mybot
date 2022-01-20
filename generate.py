69d444ac2ae566efacc88501f592f1dffd91e500
jhmarche <jhmarche@uvm.edu>
2022-01-20 18:48:23 -0500

    Box now starts on floor.

--- generate.py
+++ generate.py
@@ -8,8 +8,13 @@ length = 1
 width = 2
 height = 3
 
+#set position of cube
+x = 0
+y = 0
+z = 1.5
+
 # parameters for the box
-pyrosim.Send_Cube(name="Box", pos=[0, 0, 0.5], size=[length, width, height])
+pyrosim.Send_Cube(name="Box", pos=[x, y, z], size=[length, width, height])
 
 # stop pyrosim
 pyrosim.End()
