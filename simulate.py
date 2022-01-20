16a364c46c99acc8dd5407c867ba92f1fe3b839e
jhmarche <jhmarche@uvm.edu>
2022-01-20 18:24:34 -0500

    Added gravity to the world.

--- simulate.py
+++ simulate.py
@@ -1,10 +1,14 @@
 import pybullet as p
 import time
 
+# physics client for pybullet
 physicsClient = p.connect(p.GUI)
 
+p.setGravity(0, 0, -9.8)
+# reads in world described in box.sdf
 p.loadSDF("box.sdf")
 
+# for loop to make simulation last longer
 for x in range(1000):
     p.stepSimulation()
     time.sleep(1/60)
