867ec8021b299d71a74f9671a6b55cd4621cef9c
jhmarche <jhmarche@uvm.edu>
2022-01-21 12:58:22 -0500

    Created a leg for the robot

--- generate.py
+++ generate.py
@@ -21,6 +21,13 @@ def create_robot():
     # create torso for robot
     pyrosim.Send_Cube(name="Torso", pos=[0, 0, 0.5], size=[1, 1, 1])
 
+    # create a joint for the torso and leg to connect
+    pyrosim.Send_Joint(name="Torso_Leg", parent="Torso", child="Leg",
+                       type="revolute", position=[0.5, 0, 1])
+
+    # create a leg for robot
+    pyrosim.Send_Cube(name="Leg", pos=[1, 0, 1.5], size=[1, 1, 1])
+
     # stop pyrosim
     pyrosim.End()
 
