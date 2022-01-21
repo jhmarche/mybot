16e42f9399c52fabd23faeb2a8114d678a701f36
jhmarche <jhmarche@uvm.edu>
2022-01-20 19:20:22 -0500

    Created a huge tower of boxes that gradually get smaller as they higher.

--- generate.py
+++ generate.py
@@ -8,18 +8,23 @@ length = 1
 width = 1
 height = 1
 
-#set position of cube
+# set position of cube
 x = 0
 y = 0
 z = 0.5
 
 # creates a tower of boxes that get smaller as they get higher
-for i in range(10):
-    pyrosim.Send_Cube(name="Box", pos=[x, y, z], size=[length, width, height])
-    z += height
-    length *= 0.9
-    width *= 0.9
-    height *= 0.9
+for i in range(6):
+    for j in range(6):
+        length = 1
+        width = 1
+        height = 1
+        for k in range(10):
+            pyrosim.Send_Cube(name="Box", pos=[i, j, k], size=[length, width, height])
+            #z += height
+            length *= 0.9
+            width *= 0.9
+            height *= 0.9
 
 # stop pyrosim
 pyrosim.End()
