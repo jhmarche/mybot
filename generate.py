829a3b81fd86fac6589f70e04f0e39f42c75ee99
jhmarche <jhmarche@uvm.edu>
2022-01-21 12:38:29 -0500

    Created world with one box

--- generate.py
+++ generate.py
@@ -1,7 +1,7 @@
 import pyrosim.pyrosim as pyrosim
 
 # file to store information about world
-pyrosim.Start_SDF("boxes.sdf")
+pyrosim.Start_SDF("world.sdf")
 
 #set size of cube
 length = 1
@@ -13,18 +13,8 @@ x = 0
 y = 0
 z = 0.5
 
-# creates a tower of boxes that get smaller as they get higher
-for i in range(6):
-    for j in range(6):
-        length = 1
-        width = 1
-        height = 1
-        for k in range(10):
-            pyrosim.Send_Cube(name="Box", pos=[i, j, k], size=[length, width, height])
-            #z += height
-            length *= 0.9
-            width *= 0.9
-            height *= 0.9
+# creates a box
+pyrosim.Send_Cube(name="Box", pos=[x, y, z], size=[length, width, height])
 
 # stop pyrosim
 pyrosim.End()
