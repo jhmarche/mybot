6dcf9aaad6d974f2b896edf947b77c23817c72e1
jhmarche <jhmarche@uvm.edu>
2022-01-24 16:52:45 -0500

    Added a sensor to the robot

--- simulate.py
+++ simulate.py
@@ -1,6 +1,7 @@
 import pybullet as p
 import time
 import pybullet_data
+import pyrosim.pyrosim as pyrosim
 
 # physics client for pybullet
 physicsClient = p.connect(p.GUI)
@@ -13,9 +14,13 @@ planeId = p.loadURDF("plane.urdf")
 
 robotId = p.loadURDF("body.urdf")
 
+# used to simulate sensors
+pyrosim.Prepare_To_Simulate(robotId)
+
 # for loop to make simulation last longer
 for x in range(1000):
     p.stepSimulation()
+    backLegTouch = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
     time.sleep(1/60)
     print(x)
 
