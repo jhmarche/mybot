e3e28e45a3dabaa269680fd3bf923ff013dd6d76
jhmarche <jhmarche@uvm.edu>
2022-02-19 20:27:50 -0500

    Class hierarchy complete

--- robot.py
+++ robot.py
@@ -1,7 +1,9 @@
 from sensor import SENSOR
 from motor import MOTOR
+import pybullet as p
 
 class ROBOT:
     def __init__(self):
-        self.sensors()
-        self.motors()
+        #self.sensors()
+        #self.motors()
+        self.robotId = p.loadURDF("body.urdf")
--- simulation.py
+++ simulation.py
@@ -1,7 +1,16 @@
 from world import WORLD
 from robot import ROBOT
-
+import pybullet as p
+import constants as c
+import time
+import pybullet_data
+import pyrosim.pyrosim as pyrosim
 
 class SIMULATION:
     def __init__(self):
+        self.physicsClient = p.connect(p.GUI)
+        p.setAdditionalSearchPath(pybullet_data.getDataPath())
         self.world = WORLD()
+        self.robot = ROBOT()
+        p.setGravity(c.ZERO, c.ZERO, -9.8)
+        pyrosim.Prepare_To_Simulate(self.robot)
