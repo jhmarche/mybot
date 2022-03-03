b3835ff1e654eccc1734f87079b898a2c46a37b0
jhmarche <jhmarche@uvm.edu>
2022-03-02 21:13:22 -0500

    Added a way to simulate a synapse

--- pyrosim/neuron.py
+++ pyrosim/neuron.py
@@ -69,8 +69,20 @@ class NEURON:
     def Update_Sensor_Neuron(self):
         self.Set_Value(pyrosim.Get_Touch_Sensor_Value_For_Link(self.Get_Link_Name()))
 
-    def Update_Hidden_Or_Motor_Neuron(self):
+    def Update_Hidden_Or_Motor_Neuron(self, neurons, synapses):
         self.Set_Value(0.0)
+        print(self.Get_Value())
+        for key in synapses.keys():
+            if key[1] == self.Get_Name():
+                synapse_weight = synapses[key].Get_Weight()
+                pre_synaptic_neuron_value = neurons[key[0]].Get_Value()
+                self.Allow_Presynaptic_Neuron_To_Influence_Me(synapse_weight, pre_synaptic_neuron_value)
+        print(self.Get_Value())
+        exit()
+
+    def Allow_Presynaptic_Neuron_To_Influence_Me(self, synapse_weight, presynaptic_neuron_value):
+        result = synapse_weight * presynaptic_neuron_value
+        self.Add_To_Value(result)
 
     # -------------------------- Private methods -------------------------
 
